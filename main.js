f0479df3a79d1ffb6d785f649aba802b70b58d9b
Casey Child <cchild@atiba.com>
2021-12-29 12:13:04 -0700

    Follow examples and complete lesson 2 challege: Added description in a p tag and to data object. Extra: Use latest Vue with sri hash.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const product = 'Boots';
+const app = Vue.createApp({
+    data() {
+        return {
+            product: product,
+            description: "Comfortable; with genuine fabric made from the newest Tricoll fiber."
+        }
+    }
+});

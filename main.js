f47768f9b81b6ba936f4c09eb1a42940e66e987e
Casey Child <cchild@atiba.com>
2021-12-29 15:48:20 -0700

    Complete Lesson 8 and Challenge

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            currentVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 15, onSale: true },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: true },
+              { id: 2233, color: 'darkblue', image: './assets/images/socks_blue.jpg', quantity: 15, onSale: false },
             ]
         }
     },
@@ -17,8 +17,19 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(value) {
+            this.currentVariant = value
+        }
+    },
+    computed: {
+        image() {
+            return this.variants[this.currentVariant].image
+        },
+        inStock() {
+            return this.variants[this.currentVariant].quantity
+        },
+        title() {
+            return this.variants[this.currentVariant].onSale ? this.brand + ' ' + this.product + " (Sale)" : this.brand + ' ' + this.product;
         }
     }
 })
